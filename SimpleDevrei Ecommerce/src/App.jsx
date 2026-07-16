bafb59bed399c766259cb60d2492a5ab6b6539fe
Andrei De Jesus <andreidejesus2021@gmail.com>
2026-07-16 14:09:23 +0800

    Make an another page for Orders and copy the headers for home page

--- SimpleDevrei Ecommerce/src/App.jsx
+++ SimpleDevrei Ecommerce/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import { useState } from 'react'
 import { Home } from './pages/Home'
 import { CheckoutPage } from './pages/CheckoutPage'
+import { Orders } from "./pages/Orders";
 
 function App(){
   return(
       <Routes>
         <Route index element={<Home />}/>
         <Route path='Checkout' element={<CheckoutPage />}/>
+        <Route path='Orders' element={<Orders />}/>
       </Routes> 
   );
 }
