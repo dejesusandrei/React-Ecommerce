c2c778c98277084ba24a8a08d9e0d77de0a9be8f
Andrei De Jesus <andreidejesus2021@gmail.com>
2026-07-16 15:42:02 +0800

    Created and route the Tracking Page

--- SimpleDevrei Ecommerce/src/App.jsx
+++ SimpleDevrei Ecommerce/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { Home } from './pages/Home'
 import { CheckoutPage } from './pages/CheckoutPage'
 import { Orders } from "./pages/Orders";
+import { Tracking } from "./pages/Tracking";
 
 function App(){
   return(
@@ -10,6 +11,7 @@ function App(){
         <Route index element={<Home />}/>
         <Route path='Checkout' element={<CheckoutPage />}/>
         <Route path='Orders' element={<Orders />}/>
+        <Route path='Tracking' element={<Tracking />}/>
       </Routes> 
   );
 }
