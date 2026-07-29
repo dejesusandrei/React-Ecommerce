4956bea611c3204b555e31f19ec3357a3758e39c
Andrei De Jesus <andreidejesus2021@gmail.com>
2026-07-29 16:41:20 +0800

    Clean up for Product test

--- SimpleDevrei Ecommerce/src/tests/Product.test.jsx
+++ SimpleDevrei Ecommerce/src/tests/Product.test.jsx
@@ -1,4 +1,4 @@
-import { describe, it, expect, vi } from 'vitest'
+import { describe, it, expect, vi, beforeEach } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import { ProductCard } from '../components/Product'
 import axios from 'axios'
@@ -11,8 +11,12 @@ vi.mock('axios');
 // screen: check the fake web page
 
 describe('Product Component', () =>{
-	it('displays the product details correctly', () =>{
-		const product = {
+	let product;
+	let loadCart;
+	
+	// runs some code before each test
+	beforeEach(() =>{
+		product =  {
 			"id": "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
 			"image": "images/products/athletic-cotton-socks-6-pairs.jpg",
 			"name": "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -23,11 +27,12 @@ describe('Product Component', () =>{
 			"priceCents": 1090,
 			"keywords": ["socks", "sports", "apparel"]
 		};
-
 		// vi.fn() = create a fake function that doesnt do anything (mock)
-		const loadCart = vi.fn();
+		loadCart = vi.fn();
+	});
+	
+	it('displays the product details correctly', () =>{
 		render(<ProductCard product={product} loadCart={loadCart}/>);
-
 		expect(screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')).toBeInTheDocument();
 		expect(screen.getByText('$10.90')).toBeInTheDocument();
 		expect(screen.getByText('87')).toBeInTheDocument();
@@ -36,22 +41,7 @@ describe('Product Component', () =>{
 	});
 
 	it('add a products to cart', async () =>{
-		const product = {
-			"id": "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-			"image": "images/products/athletic-cotton-socks-6-pairs.jpg",
-			"name": "Black and Gray Athletic Cotton Socks - 6 Pairs",
-			"rating": {
-				"stars": 4.5,
-				"count": 87
-			},
-			"priceCents": 1090,
-			"keywords": ["socks", "sports", "apparel"]
-		};
-
-		// vi.fn() = create a fake function that doesnt do anything (mock)
-		const loadCart = vi.fn();
 		render(<ProductCard product={product} loadCart={loadCart}/>);
-
 		const user = userEvent.setup();
 		const addToCartBtn = screen.getByTestId('add-to-cart-button');
 		await user.click(addToCartBtn);
@@ -64,7 +54,6 @@ describe('Product Component', () =>{
 			}
 		);
 		expect(loadCart).toHaveBeenCalled();
-
 	});
 
 });
